be802c96aa44476e862bfc572639d7ab58358aac
allen spayne <allen.salapayne2001@gmail.com>
2022-10-31 22:46:06 +0800

    axios added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import { Route, Routes} from 'react-router-dom'
-import { Hero } from './components/Hero'
-import { Questions } from './components/Questions'
+import React, { useEffect, useState } from 'react';
+import { Route, Routes} from 'react-router-dom';
+import { Hero } from './components/Hero';
+import { Questions } from './components/Questions';
 import './App.scss';
+import axios from 'axios';
 
 const App = () => {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://opentdb.com/api.php?amount=10&type=multiple').then((response) => {
+      setPosts(response.data.results)
+    })
+  },[])
+
+  console.log(posts)
   return <Routes>
       <Route path="/" element={<Hero />} />
       <Route path='/questions' element={<Questions />} />
